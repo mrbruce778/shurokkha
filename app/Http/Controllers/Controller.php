c14e8792952d4eee898256161ee92b3121e411f5
mrbruce778 <brucedev@gmail.com>
2026-04-13 18:22:53 +0600

    i had nothing to say

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 abstract class Controller
 {
-    //
+    function userIDFromToken($token)
+    {
+        $user = User::where('api_token', $token)->first();
+        return $user ? $user->id : null;
+    }
 }
