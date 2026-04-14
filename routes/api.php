1469bc12fadfda658a0710181e8afe3947d5ec5e
mrbruce778 <brucedev@gmail.com>
2026-04-14 20:27:17 +0600

    changed the API name

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\AuthController;
 Route::post('/signin', [AuthController::class, 'signIn']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 Route::post('/logout', [AuthController::class, 'logout']);
-Route::patch('/update',[AuthController::class, 'updateprofile']);
+Route::patch('/updateprofile',[AuthController::class, 'updateprofile']);
